3edc342317bcc170c6c8c9eb956e1a950b31204e
Matthiiews <matthieu.kouassi@gmail.com>
2024-07-12 18:45:34 +0200

    Update django.yml file

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
