b09ae098d652fce2aea65f1bea4649622622b185
Matthiiews <matthieu.kouassi@gmail.com>
2024-07-17 15:28:39 +0200

    Add the readthedocs.yaml file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,15 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OrangeCountyLettings'
+project = 'Oclettings'
 copyright = '2024, Matthieu KOUASSI'
 author = 'Matthieu KOUASSI'
-release = '1.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
